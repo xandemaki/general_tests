454539580cc15eb4bdfbd4dd5cb7e54510fd3863
Alexandre Makiyama <amakiyama@bvmf.com.br>
2017-10-03 11:16:26 -0300

    Teste

--- cordapp-amend-test/build/tmp/expandedArchives/cordformation-0.6.3.jar_264adr77c01h76z7c3tbs5j8e/net/corda/plugins/runnodes.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-FOR /R ".\" %%G in (.) DO (
- Pushd %%G
- start java -jar corda.jar
- Popd
-)
--- cordapp-amend-test/run.bat
+++
@@ -1,4 +0,0 @@
-start /d kotlin\build\nodes\controller java -jar corda.jar
-start /d kotlin\build\nodes\nodea java -jar corda.jar
-start /d kotlin\build\nodes\nodeb java -jar corda.jar
-REM start /d kotlin\build\nodes\nodec java -jar corda.jar
